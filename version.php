b657f0794e7fb15972801cdfbdd275aabf2c6531
Willian Mano <willianmanoaraujo@gmail.com>
2023-02-25 15:14:28 -0300

    Increase version num

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->requires = 2011120511;
-$plugin->version = 2017050300;
+$plugin->version = 2023022400;
 $plugin->release = '1.0.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'block_myprogress';
